597a91127413649b5bd8bdf28f913e99ec5f372d
RidgeA <antongriadchenko@gmail.com>
2020-11-28 00:13:28 +0200

    feat: authorizer lambda

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -93,6 +93,11 @@ const serverlessConfiguration: Serverless = {
                 },
               },
             },
+            authorizer: {
+              name: 'basic-authorizer',
+              // todo: remove hardcode
+              arn: 'arn:aws:lambda:eu-west-1:625494443472:function:authorization-service-dev-basicAuthorizer:7',
+            },
           },
         },
       ],
@@ -109,6 +114,24 @@ const serverlessConfiguration: Serverless = {
           },
         },
       ],
+    },
+  },
+  resources: {
+    Resources: {
+      GatewayResponse4XX: {
+        Type: "AWS::ApiGateway::GatewayResponse",
+        Properties: {
+          ResponseParameters: {
+            'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+            'gatewayresponse.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+            'gatewayresponse.header.Access-Control-Allow-Methods': "'GET,OPTIONS'",
+          },
+          ResponseType: 'DEFAULT_4XX',
+          RestApiId: {
+            Ref: 'ApiGatewayRestApi',
+          },
+        },
+      },
     },
   },
 };
